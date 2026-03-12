8d5ecd1b63f60781e00588ff403b329d933a2f85
Akmal Ahmedh <mhakmala7@gmail.com>
2026-03-13 00:33:18 +0530

    Add Firebase config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-window.WYR_CHAOS_CONFIG = window.WYR_CHAOS_CONFIG || {
+window.WYR_CHAOS_CONFIG = {
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyBljHkivroJegedogzP8bEkYQAtmOT6Yro',
+    authDomain: 'wyr-chaos.firebaseapp.com',
+    databaseURL: 'https://wyr-chaos-default-rtdb.firebaseio.com',
+    projectId: 'wyr-chaos',
+    storageBucket: 'wyr-chaos.firebasestorage.app',
+    messagingSenderId: '191812865026',
+    appId: '1:191812865026:web:9d0e7bd323f4025701eb1b',
   },
 };
